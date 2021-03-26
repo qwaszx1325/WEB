f23ac20a00c1843de1b628c12c1c3959ef9a64d3
qwaszx1325 <48790756+qwaszx1325@users.noreply.github.com>
2021-03-26 10:56:42 +0800

    Add files via upload

---
+++ routes.py
@@ -0,0 +1,28 @@
+from flask import render_template, flash
+
+from app import app, bcrypt, db
+from froms import RegisterForm
+from models import User
+
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = bcrypt.generate_password_hash(form.password.data)
+        email = form.email.data
+        phone = form.phone.data
+        #address = form.address.data
+        user = User(username=username, email=email, password=password, phone=phone,)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congrats, registration success', category='success')
+
+    return render_template('register.html', form=form)
